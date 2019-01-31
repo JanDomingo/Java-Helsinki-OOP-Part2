f67b87e87d77cc53247f5f54eb429d270e76e811
Jan <janaarondomingo@gmail.com>
2019-01-30 20:58:41 -0800

    Exercise 9 work in progress

--- week8-week8_09.CarRegistrationCentre/src/RegistrationPlate.java
+++ week8-week8_09.CarRegistrationCentre/src/RegistrationPlate.java
@@ -15,5 +15,35 @@ public class RegistrationPlate {
     public String toString() {
         return country + " " + regCode;
     }
+    
+    public boolean equals(Object object) {
+    if (object == null) {
+        return false;
+    }
+
+    if (getClass() != object.getClass()) {
+        return false;
+    }
+
+    RegistrationPlate compared = (RegistrationPlate) object;
+
+    if (this.regCode == null || !this.regCode.equals(compared.regCode)) {
+        return false;
+    }
+
+    if (this.country == null || !this.country.equals(compared.country)) {
+        return false;
+    }
+
+    return true;
+    }
+
+    public int hashCode() {
+        if (this.regCode == null) {
+            return 7;
+        }
+
+        return this.regCode.hashCode() + this.country.hashCode();
+    }
 
 }
---
+++ week8-week8_09.CarRegistrationCentre/src/VehicleRegister.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jandomingo
+ */
+public class VehicleRegister {
+    private HashMap<RegistrationPlate, String> owners;
+    
+    public VehicleRegister() {
+        this.owners = new HashMap<RegistrationPlate, String>();
+    }
+    public boolean add(RegistrationPlate plate, String owner) {
+        if (this.owners.get)
+    }
+        
+    }
+}
