badbc6bc9849b6be53fa567579c25b89926adcb1
Jan <janaarondomingo@gmail.com>
2019-02-03 18:01:03 -0800

    Exercise 9 completed

--- week8-week8_09.CarRegistrationCentre/src/RegistrationPlate.java
+++ week8-week8_09.CarRegistrationCentre/src/RegistrationPlate.java
@@ -1,50 +1,54 @@
-
 public class RegistrationPlate {
-    // don't change the code which is already given to you
-
     // ATTENTION: the object variable types are final, meaning that their value cannot be changed!
     private final String regCode;
     private final String country;
 
-    public RegistrationPlate(String country, String regCode) {
-        this.regCode = regCode;
-        this.country = country;
+    public RegistrationPlate(String regCode, String country) {
+       this.regCode = regCode;
+       this.country = country;
     }
 
-    @Override
     public String toString() {
         return country + " " + regCode;
     }
     
-    public boolean equals(Object object) {
-    if (object == null) {
-        return false;
-    }
-
-    if (getClass() != object.getClass()) {
-        return false;
-    }
-
-    RegistrationPlate compared = (RegistrationPlate) object;
-
-    if (this.regCode == null || !this.regCode.equals(compared.regCode)) {
-        return false;
-    }
-
-    if (this.country == null || !this.country.equals(compared.country)) {
-        return false;
-    }
-
-    return true;
+    public String getRegCode() {
+        return regCode;
     }
     
-    //Look into this line 
+    public String getCountry() {
+        return country;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+        
+        RegistrationPlate compared = (RegistrationPlate) object;
+        
+        if (this.regCode != compared.regCode) {
+            return false;
+        }
+        
+        if (this.country == null || !this.country.equals(compared.country)) {
+            return false;
+        }
+        
+        return true;
+    }
+        
+    @Override
     public int hashCode() {
         if (this.regCode == null) {
             return 7;
         }
-
-        return this.regCode.hashCode() + this.country.hashCode();
+        return this.country.hashCode() + this.regCode.hashCode();
     }
-
+    
 }
--- week8-week8_09.CarRegistrationCentre/src/VehicleRegister.java
+++ week8-week8_09.CarRegistrationCentre/src/VehicleRegister.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,15 +10,55 @@ import java.util.HashMap;
  *
  * @author jandomingo
  */
+
 public class VehicleRegister {
     private HashMap<RegistrationPlate, String> owners;
     
     public VehicleRegister() {
         this.owners = new HashMap<RegistrationPlate, String>();
     }
+
     public boolean add(RegistrationPlate plate, String owner) {
-        if (this.owners.get)
+        if (!owners.containsKey(plate)) {
+            owners.put(plate, owner);
+            return true; 
+        }
+        return false; 
     }
+    
+    public String get(RegistrationPlate plate) {
+        return owners.get(plate);
+    }
+    
+    public boolean delete (RegistrationPlate plate) {
+        if(owners.containsKey(plate)) {
+            owners.remove(plate);
+            return true;
+        }
+        return false;
+    }
+    
+    public void printRegistrationPlates() {
+        for (RegistrationPlate plate : owners.keySet()) {
+            System.out.println(plate);
+        }
+    }
+    
+    public void printOwners() {
+        ArrayList<String> ownersList = new ArrayList<String>();
+        String temp = "";
         
+        for (RegistrationPlate plate : owners.keySet()) {
+            temp = owners.get(plate);
+        
+        if(!(ownersList.contains(temp))){
+            ownersList.add(temp);
+        }
+    }
+    
+        for (String owner : ownersList) {
+                System.out.println(owner);
+        }
+
     }
 }
